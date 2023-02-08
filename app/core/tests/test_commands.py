864ae384215e20135935a55086400e068c5b495f
Adam W <AdamWardVGP@gmail.com>
2023-02-08 01:58:54 -0800

    8.41 wait for DB command

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,41 @@
+"""
+Test custom Django management commands.
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTest(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database until database is ready"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for DB when OperationalError is returned"""
+        # First 2 times we call mocked method Psycopg2Error will return
+        #   the next 3 times return OperationalError. the 5th time will return "True"
+        #
+        # Postgres has different start states, depending on when the connection
+        # occurs. If the app isnt accepting connections yet you will get Psycopg2Error
+        # once its ready to accept connections but the DB is still initializing you'll get 
+        #   Operational Error
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
