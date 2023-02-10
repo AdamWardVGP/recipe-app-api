0ddf86420e2c78e70fac4379568875fd181ce654
Adam W <AdamWardVGP@gmail.com>
2023-02-10 12:54:05 -0800

    Adding missing models file

--- app/core/models.py
+++ app/core/models.py
@@ -13,6 +13,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Method to create, and store a user. New user is returned"""
+        if not email:
+            raise ValueError('User must have a vailid email')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
