a2e070acc1739f43160185894339656fac1c61eb
Adam W <AdamWardVGP@gmail.com>
2023-02-10 17:04:40 -0800

    edit user page for django dashbaord

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,7 @@ Customications for the django admin
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
 
 from core import models
 
@@ -13,6 +14,21 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users"""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = [
+        (None, {'fields': ('email', 'password')}),
+        (
+            translate('permissions'),
+            {
+                'fields' : (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (translate('important dates'), {'fields': ('last_login',)}),
+    ]
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
