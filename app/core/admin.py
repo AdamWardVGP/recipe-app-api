cfea0a603bca42a54a54491900688b6bd4d70e5f
Adam W <AdamWardVGP@gmail.com>
2023-02-10 16:19:14 -0800

    Setup user admin dashboard

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin # noqa
+"""
+Customications for the django admin
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+# we should really visit the Django admin site to learn how this works:
+# https://docs.djangoproject.com/en/3.1/ref/contrib/admin/
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
